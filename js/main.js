5995c78f53534168582556a8decbaf22e5cf4b13
Pablo Rodriguez <pablo_rodr1guez@hotmail.com>
2020-03-21 17:49:56 -0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+const contenedor = document.querySelector('#contenedor');
+
+document.querySelector('#boton-menu').addEventListener('click', () =>{
+
+
+    contenedor.classList.toggle('active');
+});
+
+
+const comprobarAncho = () =>{
+
+
+    if (window.innerWidth <= 768){
+
+        contenedor.classList.remove('active');
+    }else{
+
+        contenedor.classList.add('active');
+    }
+
+}
+
+comprobarAncho();
+
+window.addEventListener('resize', ()=>{
+
+
+    comprobarAncho();
+    
+});
